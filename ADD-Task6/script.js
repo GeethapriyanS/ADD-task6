e9c48a7c67a2fdb8e77bfca30bcd25c4596c4e69
GeethapriyanS <151654482+GeethapriyanS@users.noreply.github.com>
2023-12-15 21:12:57 +0530

    Add files task 6

---
+++ ADD-Task6/script.js
@@ -0,0 +1,13 @@
+function startAnimation() {
+    var square = document.getElementById('animatedSquare');
+
+    square.style.transform = 'translateX(200px)';
+  }
+
+  
+  function startAnimation2() {
+    var square = document.getElementById('animatedSquare2');
+    
+    square.style.transform = 'translatey(200px)';
+  
+  }
